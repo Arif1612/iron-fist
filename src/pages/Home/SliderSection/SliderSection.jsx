43fcc0cd87cc40081921dc7e88747669ed82c4f5
Md. Arif Ul Islam <arifulislam161148@gmail.com>
2023-07-20 07:56:10 +0600

    in top slider section some overlay text added

--- src/pages/Home/SliderSection/SliderSection.jsx
+++ src/pages/Home/SliderSection/SliderSection.jsx
@@ -12,24 +12,116 @@ const SliderSection = () => {
   return (
     <div>
       <Carousel>
-        <div>
-          <img src={banner1} />
+        <div className="relative">
+          <img src={banner1} alt="Banner 1" />
+          <div className="absolute inset-0 bg-black opacity-50"></div>
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-2/3 text-white ">
+            <p className="uppercase text-xl font-bold mb-3 md:text-2xl ">
+              Welcome To
+            </p>
+            <h1 className="uppercase text-4xl font-bold md:text-6xl mb-3">
+              Iron Fist Academy
+            </h1>
+            <p className="text-xl font-bold uppercase md:text-2xl mb-4">
+              Train | Belong | Evolve
+            </p>
+            <button className="btn px-6 py-4 hover:bg-gray-600 hover:text-white font-bold uppercase ">
+              Contact us today
+            </button>
+          </div>
         </div>
-        <div>
-          <img src={banner2} />
+        <div className="relative">
+          <img src={banner2} alt="Banner 2" />
+          <div className="absolute inset-0 bg-black opacity-50"></div>
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-2/3 text-white ">
+            <p className="uppercase text-xl font-bold mb-3 md:text-2xl ">
+              Welcome To
+            </p>
+            <h1 className="uppercase text-4xl font-bold md:text-6xl mb-3">
+              Iron Fist Academy
+            </h1>
+            <p className="text-xl font-bold uppercase md:text-2xl mb-4">
+              Train | Belong | Evolve
+            </p>
+            <button className="btn px-6 py-4 hover:bg-gray-600 hover:text-white font-bold uppercase ">
+              Contact us today
+            </button>
+          </div>
         </div>
-        <div>
-          <img src={banner3} />
+        <div className="relative">
+          <img src={banner3} alt="Banner 3" />
+          <div className="absolute inset-0 bg-black opacity-50"></div>
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-2/3 text-white ">
+            <p className="uppercase text-xl font-bold mb-3 md:text-2xl ">
+              Welcome To
+            </p>
+            <h1 className="uppercase text-4xl font-bold md:text-6xl mb-3">
+              Iron Fist Academy
+            </h1>
+            <p className="text-xl font-bold uppercase md:text-2xl mb-4">
+              Train | Belong | Evolve
+            </p>
+            <button className="btn px-6 py-4 hover:bg-gray-600 hover:text-white font-bold uppercase ">
+              Contact us today
+            </button>
+          </div>
         </div>
-        <div>
-          <img src={banner4} />
+        <div className="relative">
+          <img src={banner4} alt="Banner 4" />
+          <div className="absolute inset-0 bg-black opacity-50"></div>
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-2/3 text-white ">
+            <p className="uppercase text-xl font-bold mb-3 md:text-2xl ">
+              Welcome To
+            </p>
+            <h1 className="uppercase text-4xl font-bold md:text-6xl mb-3">
+              Iron Fist Academy
+            </h1>
+            <p className="text-xl font-bold uppercase md:text-2xl mb-4">
+              Train | Belong | Evolve
+            </p>
+            <button className="btn px-6 py-4 hover:bg-gray-600 hover:text-white font-bold uppercase ">
+              Contact us today
+            </button>
+          </div>
         </div>
-        <div>
-          <img src={banner5} />
+        <div className="relative">
+          <img src={banner5} alt="Banner 5" />
+          <div className="absolute inset-0 bg-black opacity-50"></div>
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-2/3 text-white ">
+            <p className="uppercase text-xl font-bold mb-3 md:text-2xl ">
+              Welcome To
+            </p>
+            <h1 className="uppercase text-4xl font-bold md:text-6xl mb-3">
+              Iron Fist Academy
+            </h1>
+            <p className="text-xl font-bold uppercase md:text-2xl mb-4">
+              Train | Belong | Evolve
+            </p>
+            <button className="btn px-6 py-4 hover:bg-gray-600 hover:text-white font-bold uppercase ">
+              Contact us today
+            </button>
+          </div>
         </div>
-        <div>
-          <img src={banner6} />
+        <div className="relative">
+          <img src={banner6} alt="Banner 6" />
+          <div className="absolute inset-0 bg-black opacity-50"></div>
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-2/3 text-white ">
+            <p className="uppercase text-xl font-bold mb-3 md:text-2xl ">
+              Welcome To
+            </p>
+            <h1 className="uppercase text-4xl font-bold md:text-6xl mb-3">
+              Iron Fist Academy
+            </h1>
+            <p className="text-xl font-bold uppercase md:text-2xl mb-4">
+              Train | Belong | Evolve
+            </p>
+            <button className="btn px-6 py-4 hover:bg-gray-600 hover:text-white font-bold uppercase ">
+              Contact us today
+            </button>
+          </div>
         </div>
+
+       
       </Carousel>
     </div>
   );
